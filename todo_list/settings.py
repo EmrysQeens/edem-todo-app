d8462fe6b89deeb3dbc297b3c35da06f5dc241a2
emrys qeens <emrysqeens@gmail.com>
2021-05-17 14:22:32 +0100

    added whitenoise

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
+
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+fog592d*gv*_p*1_k2c92c1$9)%nttt*wkgtllis*3fzryc2%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] if DEBUG else ['*']
 
 
 # Application definition
@@ -63,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+		'whitenoise.middleware.WhiteNoiseMiddleware',
             ],
         },
     },
@@ -74,12 +77,18 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
+DATABASES ={
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+    } if DEBUG else {
+	'USER': os.getenv('USER'),
+	'PORT': 5432,
+	'HOST': os.getenv('HOST'),
+	'ENGINE': 'django.db.backends.postgresql_psycopg2',
+	'NAME': os.getenv('DATABASE'),
+	'PASSWORD': os.getenv('PASSWORD'),
+}}
 
 
 # Password validation
@@ -116,10 +125,22 @@ USE_TZ = True
 
 LOGIN_URL='login'
 
+APPEND_SLASH = True
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIR = [
+    'static',
+    '/static/',
+]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
